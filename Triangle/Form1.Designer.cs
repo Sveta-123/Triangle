7f56dd6b5599e65e412d584fe985bc87d01fc8cf
svetl <svetl@LAPTOP-OGC17F07>
2023-10-24 10:29:53 +0300

    Добавьте файлы проекта.

---
+++ Triangle/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Triangle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRez = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelRez = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonRez
+            // 
+            this.buttonRez.BackColor = System.Drawing.Color.Gold;
+            this.buttonRez.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRez.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonRez.Location = new System.Drawing.Point(158, 214);
+            this.buttonRez.Name = "buttonRez";
+            this.buttonRez.Size = new System.Drawing.Size(295, 52);
+            this.buttonRez.TabIndex = 0;
+            this.buttonRez.Text = "Проверить";
+            this.buttonRez.UseVisualStyleBackColor = false;
+            this.buttonRez.Click += new System.EventHandler(this.buttonRez_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Сторона A:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(258, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Сторона B:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(484, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 30);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Сторона C:";
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBoxA.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxA.Location = new System.Drawing.Point(33, 136);
+            this.textBoxA.Multiline = true;
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(134, 36);
+            this.textBoxA.TabIndex = 4;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBoxB.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxB.Location = new System.Drawing.Point(255, 136);
+            this.textBoxB.Multiline = true;
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(137, 36);
+            this.textBoxB.TabIndex = 5;
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBoxC.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxC.Location = new System.Drawing.Point(489, 133);
+            this.textBoxC.Multiline = true;
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(126, 36);
+            this.textBoxC.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(115, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(430, 30);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Введите длины сторон треугольника";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Window;
+            this.label5.Location = new System.Drawing.Point(408, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.TabIndex = 8;
+            // 
+            // labelRez
+            // 
+            this.labelRez.AutoSize = true;
+            this.labelRez.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRez.Location = new System.Drawing.Point(115, 293);
+            this.labelRez.Name = "labelRez";
+            this.labelRez.Size = new System.Drawing.Size(0, 30);
+            this.labelRez.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(663, 361);
+            this.Controls.Add(this.labelRez);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonRez);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Треугольник";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRez;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelRez;
+    }
+}
+
